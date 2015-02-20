e9a663bd575dc9f3f556d2e26393df761828f49f
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2015-02-20 17:23:48 +0300

    qq1 add

---
+++ src/a/b/c/QQ1.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 2/20/2015.
+ * Project: jbtester_test00
+ * *******************************
+ */
+public class QQ1 {
+}
