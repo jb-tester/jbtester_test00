056bb6b5ded9cc458cd1988700217985ba43eae9
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2015-11-27 14:01:11 +0300

    merge
    Conflicts:
    	src/a/b/c/Q1.java
    	src/a/b/c/Q2.java

---
+++ src/d/e/f/AAA.java
@@ -0,0 +1,10 @@
+package d.e.f;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 11/27/2015.
+ * Project: jbtester_test00
+ * *******************************
+ */
+public class AAA {
+}
